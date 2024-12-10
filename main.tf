d0a1e9bad5924ba13ae595e31197fe4310688142
Dima <zlotea_dumitru@yahoo.com>
2024-12-10 18:55:17 -0500

    deploying prometheus and grafana with terraform

--- main.tf
+++ main.tf
@@ -11,20 +11,6 @@ provider "azurerm" {
   features {}
 }
 
-# Create Log analytics workspace
-resource "azurerm_log_analytics_workspace" "log_analytics" {
-  name                  = "aks-log-workspace"
-  location              = "Canada Central"
-  resource_group_name   = "rg-webgoat"
-  sku                   = "PerGB2018"
-  retention_in_days     = 30
-  
-  tags = {
-    environment = "Develop"
-  }
-}
-
-
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = "webGoatCluster"
   location            = "Canada Central"
@@ -45,12 +31,190 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
   role_based_access_control_enabled = true
 
-# Link AKS with log analytics for monitoring
-  oms_agent {
-    log_analytics_workspace_id = azurerm_log_analytics_workspace.log_analytics.id
+  monitor_metrics {
+    annotations_allowed = "prometheus.io/scrape,prometheus.io/path"
+    labels_allowed      = "app,environment,version"
   }
 
   tags = {
     environment = "Develop"
+  }
+}
+
+# Create Log analytics workspace
+resource "azurerm_monitor_workspace" "amw" {
+  name                  = "aks-monitor-workspace"
+  location              = "Canada Central"
+  resource_group_name   = "rg-webgoat"
+}
+
+resource "azurerm_monitor_data_collection_endpoint" "dce" {
+  name                  = "dataCollectionEndpoint"
+  resource_group_name   = "rg-webgoat"
+  location              = "Canada Central"
+  kind                  = "Linux"
+}
+
+resource "azurerm_monitor_data_collection_rule" "dcr" {
+  name                          = "dataCollectionRule"
+  resource_group_name           = "rg-webgoat"
+  location                      = "Canada Central"
+  data_collection_endpoint_id   = azurerm_monitor_data_collection_endpoint.dce.id
+  kind                          = "Linux"
+
+  destinations {
+    monitor_account {
+      monitor_account_id    = azurerm_monitor_workspace.amw.id
+      name                  = "MonitoringAccount1"
+    }
+  }
+
+  data_flow {
+    streams         = [ "Microsoft-PrometheusMetrics" ]
+    destinations    = [ "MonitoringAccount1" ]
+  }
+
+  data_sources {
+    prometheus_forwarder {
+      streams   = [ "Microsoft-PrometheusMetrics" ]
+      name      = abs("PrometheusDataSource")
+    }
+  }
+
+  description = "DCR for Azure Monitor Metrics Profile (Managed Prometheus)"
+  depends_on = [ 
+    azurerm_monitor_data_collection_endpoint.dce
+  ]
+}
+
+resource "azurerm_monitor_data_collection_rule_association" "dcra" {
+  name                    = "MSProm"
+  target_resource_id      = azurerm_kubernetes_cluster.aks.id
+  data_collection_rule_id = azurerm_monitor_data_collection_rule.dcr.id
+  description             = "Association of data collection rule. Deleting this association will break the data collection for this AKS Cluster."
+  depends_on = [
+    azurerm_monitor_data_collection_rule.dcr
+  ]
+}
+
+resource "azurerm_dashboard_grafana" "grafana" {
+  name                  = "grafana-prometheus"
+  resource_group_name   = "rg-webgoat"
+  location              = "Canada Central"
+  grafana_major_version = "10"
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  azure_monitor_workspace_integrations {
+    resource_id = azurerm_monitor_workspace.amw.id
+  }
+}
+
+resource "azurerm_role_assignment" "datareaderrole" {
+  scope              = azurerm_monitor_workspace.amw.id
+  role_definition_id = "/subscriptions/${split("/", azurerm_monitor_workspace.amw.id)[2]}/providers/Microsoft.Authorization/roleDefinitions/b0d8363b-8ddd-447d-831f-62ca05bff136"
+  principal_id       = azurerm_dashboard_grafana.grafana.identity.0.principal_id
+}
+
+resource "azurerm_monitor_alert_prometheus_rule_group" "node_recording_rules_rule_group" {
+  name                = "NodeRecordingRulesRuleGroup-webGoatCluster"
+  location            = "Canada Central"
+  resource_group_name = "rg-webgoat"
+  cluster_name        = "webGoatCluster"
+  description         = "Node Recording Rules Rule Group"
+  rule_group_enabled  = true
+  interval            = "PT1M"
+  scopes              = [azurerm_monitor_workspace.amw.id,azurerm_kubernetes_cluster.aks.id]
+
+  rule {
+    enabled    = true
+    record     = "instance:node_num_cpu:sum"
+    expression = <<EOF
+count without (cpu, mode) (  node_cpu_seconds_total{job="node",mode="idle"})
+EOF
+  }
+  rule {
+    enabled    = true
+    record     = "instance:node_cpu_utilisation:rate5m"
+    expression = <<EOF
+1 - avg without (cpu) (  sum without (mode) (rate(node_cpu_seconds_total{job="node", mode=~"idle|iowait|steal"}[5m])))
+EOF
+  }
+  rule {
+    enabled    = true
+    record     = "instance:node_load1_per_cpu:ratio"
+    expression = <<EOF
+(  node_load1{job="node"}/  instance:node_num_cpu:sum{job="node"})
+EOF
+  }
+  rule {
+    enabled    = true
+    record     = "instance:node_memory_utilisation:ratio"
+    expression = <<EOF
+1 - (  (    node_memory_MemAvailable_bytes{job="node"}    or    (      node_memory_Buffers_bytes{job="node"}      +      node_memory_Cached_bytes{job="node"}      +      node_memory_MemFree_bytes{job="node"}      +      node_memory_Slab_bytes{job="node"}    )  )/  node_memory_MemTotal_bytes{job="node"})
+EOF
+  }
+}
+
+resource "azurerm_monitor_alert_prometheus_rule_group" "kubernetes_recording_rules_rule_group" {
+  name                = "KubernetesRecordingRulesRuleGroup-webGoatCluster"
+  location            = "Canada Central"
+  resource_group_name = "rg-webgoat"
+  cluster_name        = "webGoatCluster"
+  description         = "Kubernetes Recording Rules Rule Group"
+  rule_group_enabled  = true
+  interval            = "PT1M"
+  scopes              = [azurerm_monitor_workspace.amw.id,azurerm_kubernetes_cluster.aks.id]
+
+  rule {
+    enabled    = true
+    record     = "node_namespace_pod_container:container_cpu_usage_seconds_total:sum_irate"
+    expression = <<EOF
+sum by (cluster, namespace, pod, container) (  irate(container_cpu_usage_seconds_total{job="cadvisor", image!=""}[5m])) * on (cluster, namespace, pod) group_left(node) topk by (cluster, namespace, pod) (  1, max by(cluster, namespace, pod, node) (kube_pod_info{node!=""}))
+EOF
+  }
+  rule {
+    enabled    = true
+    record     = "node_namespace_pod_container:container_memory_working_set_bytes"
+    expression = <<EOF
+container_memory_working_set_bytes{job="cadvisor", image!=""}* on (namespace, pod) group_left(node) topk by(namespace, pod) (1,  max by(namespace, pod, node) (kube_pod_info{node!=""}))
+EOF
+  }
+  rule {
+    enabled    = true
+    record     = "cluster:namespace:pod_memory:active:kube_pod_container_resource_requests"
+    expression = <<EOF
+kube_pod_container_resource_requests{resource="memory",job="kube-state-metrics"}  * on (namespace, pod, cluster)group_left() max by (namespace, pod, cluster) (  (kube_pod_status_phase{phase=~"Pending|Running"} == 1))
+EOF
+  }
+  rule {
+    enabled    = true
+    record     = "cluster:namespace:pod_cpu:active:kube_pod_container_resource_requests"
+    expression = <<EOF
+kube_pod_container_resource_requests{resource="cpu",job="kube-state-metrics"}  * on (namespace, pod, cluster)group_left() max by (namespace, pod, cluster) (  (kube_pod_status_phase{phase=~"Pending|Running"} == 1))
+EOF
+  }
+  rule {
+    enabled    = true
+    record     = "cluster:namespace:pod_cpu:active:kube_pod_container_resource_limits"
+    expression = <<EOF
+kube_pod_container_resource_limits{resource="cpu",job="kube-state-metrics"}  * on (namespace, pod, cluster)group_left() max by (namespace, pod, cluster) ( (kube_pod_status_phase{phase=~"Pending|Running"} == 1) )
+EOF
+  }
+  rule {
+    enabled    = true
+    record     = "namespace_cpu:kube_pod_container_resource_limits:sum"
+    expression = <<EOF
+sum by (namespace, cluster) (    sum by (namespace, pod, cluster) (        max by (namespace, pod, container, cluster) (          kube_pod_container_resource_limits{resource="cpu",job="kube-state-metrics"}        ) * on(namespace, pod, cluster) group_left() max by (namespace, pod, cluster) (          kube_pod_status_phase{phase=~"Pending|Running"} == 1        )    ))
+EOF
+  }
+  rule {
+    enabled    = true
+    record     = "cluster:node_cpu:ratio_rate5m"
+    expression = <<EOF
+sum(rate(node_cpu_seconds_total{job="node",mode!="idle",mode!="iowait",mode!="steal"}[5m])) by (cluster) /count(sum(node_cpu_seconds_total{job="node"}) by (cluster, instance, cpu)) by (cluster)
+EOF
   }
 }
